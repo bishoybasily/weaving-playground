de42c08e9aecd1b22a091e7320f3572c1980f664
bishoybasily <bishoybasily@gmail.com>
2025-06-05 13:49:39 +0200

    fix plugin warnings aspectjrt applied to module aspectjweaver applied to weaver module

--- buildSrc/src/main/groovy/ConventionsWeaving.groovy
+++ buildSrc/src/main/groovy/ConventionsWeaving.groovy
@@ -8,37 +8,30 @@ class ConventionsWeaving implements Plugin<Project> {
         def moduleName = module.name
         if (moduleName.endsWith("-weaver")) {
             // this is a weaver module
-
+            def wovenModuleName = moduleName.substring(0, moduleName.lastIndexOf("-weaver"))
             // 1- find the corresponding woven module of this module
-            def wovenModule = module.rootProject.allprojects.find { it.name == moduleName.substring(0, moduleName.lastIndexOf("-weaver")) }
-
+            def wovenModule = module.rootProject.allprojects.find { it.name == wovenModuleName }
+            // 2- if a woven module is found, only include its main sources "compileOnly"
             if (wovenModule) {
-
-                module.pluginManager.apply('io.freefair.aspectj.post-compile-weaving')
                 module.dependencies {
-
-                    implementation 'org.aspectj:aspectjrt:1.9.22.1'
-                    compileOnly 'org.aspectj:aspectjweaver:1.9.22.1'
+                    compileOnly 'org.aspectj:aspectjweaver'
                     // not necessary but a nice to have for the ide to be aware of the classes being woven
                     compileOnly module.files("${wovenModule.buildDir.path}/classes/java/main")
                 }
-
             }
-
         } else {
             // the module to be woven
-
+            def weaverModuleName = "${moduleName}-weaver"
             // 1- find the corresponding weaver module of this module
-            def weaverModule = module.rootProject.allprojects.find { it.name == "${moduleName}-weaver" }
-
-            // 2- if a weaver module is found, apply it along with freefair plugin
+            def weaverModule = module.rootProject.allprojects.find { it.name == weaverModuleName }
+            // 2- if a weaver module is found, apply it this module
             if (weaverModule) {
-
                 module.pluginManager.apply('io.freefair.aspectj.post-compile-weaving')
                 module.dependencies {
                     aspect weaverModule
                     implementation weaverModule
                     testImplementation weaverModule
+                    implementation 'org.aspectj:aspectjrt'
                 }
                 module.tasks.named('compileJava') {
                     ajc.options.compilerArgs += "-showWeaveInfo"
@@ -46,9 +39,7 @@ class ConventionsWeaving implements Plugin<Project> {
                     ajc.options.compilerArgs += "-Xlint:adviceDidNotMatch=error"
                 }
             }
-
         }
-
 
     }
 }
