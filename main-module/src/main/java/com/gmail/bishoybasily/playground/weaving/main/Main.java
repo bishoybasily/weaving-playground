12c1dd9d25941511cd8dcddd981808f56f0917c5
bishoybasily <bishoybasily@gmail.com>
2025-09-30 05:03:13 +0200

    cleanup

--- main-module/src/main/java/com/gmail/bishoybasily/playground/weaving/main/Main.java
+++ main-module/src/main/java/com/gmail/bishoybasily/playground/weaving/main/Main.java
@@ -1,16 +1,13 @@
 package com.gmail.bishoybasily.playground.weaving.main;
 
-import static com.gmail.bishoybasily.playground.weaving.FeatureFlags.FEATURE_CALCULATOR_SUBTRACT;
-import static com.gmail.bishoybasily.playground.weaving.FeatureFlags.enable;
-
 import com.gmail.bishoybasily.playground.weaving.calculator.Calculator;
 
 public class Main {
   public static void main(String[] args) {
 
-    enable(FEATURE_CALCULATOR_SUBTRACT);
 
-    var calculator = new Calculator();
-    System.out.println(calculator.add("5", "3"));
+        var calculator = new Calculator();
+        System.out.println(calculator.add("5", "3"));
+
   }
 }
