0cd804533bb4b94d4a8772bbe4e4ed9ab70b3efc
bishoy basily <34089149+bishoybasily@users.noreply.github.com>
2025-10-05 15:11:20 +0200

    weaver-sources-access (#2)
    weavingCompleteImplementation

---
+++ app-dist/src/test/java/com/gmail/bishoybasily/playground/weaving/app/WeavingPlaygroundApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gmail.bishoybasily.playground.weaving.app;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeavingPlaygroundApplicationTests {
+
+  @Autowired JobService jobService;
+
+  @Test
+  void contextLoads() {
+    Assertions.assertThat(jobService.getNumJobs()).isEqualTo(50);
+  }
+}
