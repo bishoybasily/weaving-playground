4914765a2e891c74f14c0c13c58b67c4c93f7763
bishoybasily <bishoybasily@gmail.com>
2025-10-05 15:09:21 +0200

    weavingCompleteImplementation

--- app-dist/src/main/java/com/gmail/bishoybasily/playground/weaving/app/DeploymentServiceDistImpl.java
+++ app-dist/src/main/java/com/gmail/bishoybasily/playground/weaving/app/DeploymentServiceDistImpl.java
@@ -1,8 +1,8 @@
 package com.gmail.bishoybasily.playground.weaving.app;
 
 public class DeploymentServiceDistImpl implements DeploymentService {
-    @Override
-    public void print() {
-        System.out.print("I'm the custom deployment service");
-    }
+  @Override
+  public void print() {
+    System.out.println("I'm the custom deployment service");
+  }
 }
--- app-dist/src/main/java/com/gmail/bishoybasily/playground/weaving/app/DistConfig.java
+++ app-dist/src/main/java/com/gmail/bishoybasily/playground/weaving/app/DistConfig.java
@@ -1,16 +1,15 @@
 package com.gmail.bishoybasily.playground.weaving.app;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-//@Configuration
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Primary;
+//
+// @Configuration
 public class DistConfig {
 
-//    @Bean
-//    @Primary
-    DeploymentService deploymentService() {
-        return new DeploymentServiceDistImpl();
-    }
-
+  //  @Bean
+  //  @Primary
+  DeploymentService deploymentService() {
+    return new DeploymentServiceDistImpl();
+  }
 }
